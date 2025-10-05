40d1babe485252c0f1ad58cc7ffcfd9d6964f335
1007asari1804 <1007asari1804@gmail.com>
2025-10-05 19:30:23 +0900

    検索ワードの入力フォームを設置する

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -1,4 +1,11 @@
 <x-login-layout>
 
+<div class="search-container">
+  <form action="/search" method="post">
+    @csrf
+    <input type="text" name="keyword" class="form" placeholder="ユーザー名">
+    <button class="search-button" type="submit"><img src="images/search.png"></button>
+  </form>
+</div>
 
 </x-login-layout>
