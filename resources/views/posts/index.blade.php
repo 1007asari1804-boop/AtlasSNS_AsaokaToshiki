10e4dbb6ab15ed732306e18212070f1b1cbaff5a
1007asari1804 <1007asari1804@gmail.com>
2025-10-05 19:30:48 +0900

    投稿リスト（仮）

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,5 +6,39 @@
   <button class="post-button"><img src="images/post.png"><a href="/top"></a></button>
 </div>
 
+<div>
+  <ul>
+    <li class="post-block">
+      <figure><img src="https://placehold.jp/50x50.png" alt="Aさん"></figure>
+      <div class="post-content">
+        <div>
+          <div class="post-name">Aさん</div>
+          <div>2022-12-22</div>
+        </div>
+        <div>投稿内容<br>投稿内容</div>
+      </div>
+    </li>
+    <li class="post-block">
+      <figure><img src="https://placehold.jp/50x50.png" alt="Bさん"></figure>
+      <div class="post-content">
+        <div>
+          <div class="post-name">Bさん</div>
+          <div>2022-12-22</div>
+        </div>
+        <div>投稿内容<br>投稿内容</div>
+      </div>
+    </li>
+    <li class="post-block">
+      <figure><img src="https://placehold.jp/50x50.png" alt="Cさん"></figure>
+      <div class="post-content">
+        <div>
+          <div class="post-name">Cさん</div>
+          <div>2022-12-22</div>
+        </div>
+        <div>投稿内容<br>投稿内容</div>
+      </div>
+    </li>
+  </ul>
+</div>
 
 </x-login-layout>
