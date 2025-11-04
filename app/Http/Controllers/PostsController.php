fac5a838cf592e6314855907a434a32aeae4a442
1007asari1804 <1007asari1804@gmail.com>
2025-11-04 19:24:42 +0900

    投稿機能

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,18 +5,32 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\PostFormRequest;
 
 
 class PostsController extends Controller
 {
     //
     public function index(){
-        return view('posts.index');
+        $posts = Post::orderBy('id', 'desc')->get();
+        return view('posts.index', ['posts' => $posts]);
     }
 
-    public function postCreate(Request $request){
+    public function postCreate(PostFormRequest $request){
         $post = $request->input('post');
         Post::create(['user_id' => Auth::id(), 'post' => $post]);
+        return back();
+    }
+
+    public function update(Request $request){
+        $id = $request->input('id');
+        $up_post = $request->input('upPost');
+        Post::where('id', $id)->update(['post' => $up_post]);
+        return redirect('/top');
+    }
+
+    public function delete($id){
+        Post::where('id', $id)->delete();
         return back();
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,46 +4,34 @@
   <img src="images/icon1.png">
   {{ Form::open(['url' => '/posts/create']) }}
   <div>
-    {{ Form::input('text', 'post', null, ['placeholder' => '投稿内容を入力してください。']) }}
+    {{ Form::textarea('post', null, ['rows' => 4, 'placeholder' => '投稿内容を入力してください。']) }}
+    @error('post')
+    <span>{{ $message }}</span>
+    @enderror
   </div>
   <!-- <input type="text" name="post" placeholder="投稿内容を入力してください。"> -->
-  <button class="post-button" type='submit'><img src="images/post.png"></button>
+  <button class="post-button" type='submit'><img src="{{ asset("images/post.png") }}"></button>
   {{ Form::close() }}
 </div>
 
 <div>
   <ul>
+    @foreach($posts as $post)
     <li class="post-block">
-      <figure><img src="https://placehold.jp/50x50.png" alt="Aさん"></figure>
+      <figure><img src="{{ asset("images/{$post->user->icon_image}") }}" alt="{{ $post->user->username }}"></figure>
       <div class="post-content">
         <div>
-          <div class="post-name">Aさん</div>
-          <div>2022-12-22</div>
+          <div class="post-name">{{ $post->user->username }}</div>
+          <div>{{ $post->created_at->format('Y-m-d') }}</div>
         </div>
-        <div>投稿内容<br>投稿内容</div>
+        <div>{!! nl2br(e($post->post)) !!}</div>
       </div>
-    </li>
-    <li class="post-block">
-      <figure><img src="https://placehold.jp/50x50.png" alt="Bさん"></figure>
-      <div class="post-content">
-        <div>
-          <div class="post-name">Bさん</div>
-          <div>2022-12-22</div>
-        </div>
-        <div>投稿内容<br>投稿内容</div>
-      </div>
-    </li>
-    <li class="post-block">
-      <figure><img src="https://placehold.jp/50x50.png" alt="Cさん"></figure>
-      <div class="post-content">
-        <div>
-          <div class="post-name">Cさん</div>
-          <div>2022-12-22</div>
-        </div>
-        <div>投稿内容<br>投稿内容</div>
-      </div>
+      @if($post->user_id === Auth::id())
       <a class="js-modal-open" post="{{ $post->post }}" post_id="{{ $post->id }}"><img src="{{ asset("images/edit.png") }}" alt="編集"></a>
+      <a href="/posts/{{$post->id}}/delete" onclick="return confirm('この投稿を削除します。よろしいでしょうか？')"><img src="{{ asset("images/trash.png") }}" alt="削除"></a>
+      @endif
     </li>
+    @endforeach
   </ul>
 </div>
 
@@ -57,4 +45,5 @@
     {{ Form::close() }}
   </div>
 </div>
+
 </x-login-layout>
